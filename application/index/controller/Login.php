5244124862373599bed98d04acf61b70aa23d626
tgy <718844739@qq.com>
2017-12-06 23:59:44 +0800

    fix

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -4,12 +4,14 @@ namespace app\index\controller;
 use app\index\model\User;
 use QCloud_WeApp_SDK\Auth\LoginService as LoginService;
 use QCloud_WeApp_SDK\Constants as Constants;
+use think\Log as log;
 class Login extends Base
 {
     public function index(){
         $result = LoginService::login();
         if ($result['loginState'] === Constants::S_AUTH) {
             $info = $result["userinfo"];
+            log::info($info);
             $model = new User();
             $user = $model->where("openId", $info["openId"])->find();
             if(!$user) {
